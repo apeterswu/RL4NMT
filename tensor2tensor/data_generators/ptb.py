bd3ab96b6a1e1f6402f6399686e838c874ada1a8
TrellixVulnTeam <charles.mcfarland@trellix.com>
2022-10-22 08:29:48 +0000

    Adding tarfile member sanitization to extractall()

--- tensor2tensor/data_generators/ptb.py
+++ tensor2tensor/data_generators/ptb.py
@@ -122,7 +122,26 @@ class PTBProblem(problem.Text2TextProblem):
             ptb_files += [m.name]
           files += [m]
 
-      tgz.extractall(tmp_dir, members=files)
+      def is_within_directory(directory, target):
+          
+          abs_directory = os.path.abspath(directory)
+          abs_target = os.path.abspath(target)
+      
+          prefix = os.path.commonprefix([abs_directory, abs_target])
+          
+          return prefix == abs_directory
+      
+      def safe_extract(tar, path=".", members=None, *, numeric_owner=False):
+      
+          for member in tar.getmembers():
+              member_path = os.path.join(path, member.name)
+              if not is_within_directory(path, member_path):
+                  raise Exception("Attempted Path Traversal in Tar File")
+      
+          tar.extractall(path, members, numeric_owner=numeric_owner) 
+          
+      
+      safe_extract(tgz, tmp_dir, members=files)
 
     if self.is_character_level:
       files = ptb_char_files
